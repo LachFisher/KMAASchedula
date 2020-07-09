2fd968ddbe3b66ed0134c9a3e7d075560f7a23f0
LachFisher <lachfisher37@gmail.com>
2020-07-09 15:29:47 +1000

    Initial Setup

---
+++ JS/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDy4GFrKaGp58ZOfCJDiE2yzi14P61QTgI",
+    authDomain: "kmaa-scheduling.firebaseapp.com",
+    databaseURL: "https://kmaa-scheduling.firebaseio.com",
+    projectId: "kmaa-scheduling",
+    storageBucket: "kmaa-scheduling.appspot.com",
+    messagingSenderId: "308877948500",
+    appId: "1:308877948500:web:9390d3232b56f42fa59fc1",
+    measurementId: "G-QMNFC16EC1"
+};
